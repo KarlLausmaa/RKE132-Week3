c29cadc04d25b5cc29f62afc6d6754a54f458f26
Karl Lausmaa <karl.lausmaa@gmail.com>
2023-03-29 18:00:07 +0300

    switch case

---
+++ DayOfTheWeek/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+
+int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+if (weekDay == 0)
+{
+    Console.WriteLine("Sunday");
+} else if (weekDay == 1)
+{
+    Console.WriteLine("Monday");
+}
+else if (weekDay == 2)
+{
+    Console.WriteLine("Tuestday");
+}
+else if (weekDay == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+else if (weekDay == 4)
+{
+    Console.WriteLine("Thursday");
+}
+else if (weekDay == 5)
+{
+    Console.WriteLine("Friday");
+}
+else 
+{
+    Console.WriteLine("Saturday");
+}
+
+Console.WriteLine("Have a nice day!");
---
+++ SwitchDayOfTheWeek/Program.cs
@@ -0,0 +1,31 @@
+﻿int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+switch (weekDay)
+{
+    case 0:
+        Console.WriteLine("Sunday");
+        break;
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuseday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+    default:
+        Console.WriteLine("Your calendar is broken :(");
+        break;
+}
+
+Console.WriteLine("Have a nice day");
